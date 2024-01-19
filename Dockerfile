4b6329ed21b693eece8c1aa70aa04d1bb0daf2cc
Coco Matthey <coco.matthey@sap.com>
2024-01-19 15:54:45 -0800

    fixup! First commit

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,21 @@
 FROM python:3.9-slim
 
+RUN apt-get update -qq && apt-get install ffmpeg -y
+
 # Create a working directory
 WORKDIR /app
-
 # Copy requirements file
 COPY requirements.txt .
+EXPOSE 7860
 
 # Install dependencies
 RUN pip install -r requirements.txt
-
 # Gradio app file
 COPY app.py .
 
-EXPOSE 7860
+# Environment variables
+ENV GUNICORN_CMD_ARGS="--limit-request-line 0 --limit-request-field_size 0"
 
 # Set the command to run the app
 CMD ["python", "app.py"]
-
 
--- app.py
+++ app.py
@@ -1,35 +1,46 @@
-# A simple gradio UI to provide whisper, https://github.com/openai/whisper powered transcribe
+# Whisper 簡單示範 - https://github.com/openai/whisper
+
+import logging
+import traceback
 
 import gradio as gr
 import torch
 import whisper
 
-# INITIALIZE ENVIRONMENT
+logging.basicConfig(filename='whisper_app.log', level=logging.INFO)
+
 USE_GPU = True if torch.cuda.is_available() else False
 use_fp16 = True if USE_GPU else False
 DEVICE = "cuda" if USE_GPU else "cpu"
 
 
 def launch():
-    options = ["base", "small", "medium", "large"]
+    options = ["tiny", "base", "small", "medium", "large"]
     interface = gr.Interface(
-        fn=transcribe,  # The function to run
-        inputs=["file", gr.Dropdown(options)],
+        fn=transcribe,
+        inputs=["file", gr.Dropdown(choices=options, value="base")],
         outputs=["textbox"],
         title="Whisper 簡單示範",
         description="上傳音訊檔案 - 聽打",
     )
-    interface.launch(server_name="0.0.0.0", share=False)  # Make it accessible outside the container
+    interface.launch(server_name="0.0.0.0", share=True, debug=True,
+                     show_error=True)  # Make it accessible outside the container
 
 
-def transcribe(file, choice):
-    model = whisper.load_model(choice, device=DEVICE)
+def transcribe(name, model_size):
     try:
-        result = model.transcribe(file, fp16=use_fp16)
+        model = whisper.load_model(model_size, device=DEVICE)
+        result = model.transcribe(name, fp16=use_fp16)
+        logging.info(result)
         return result["text"]
     except:
-        return ""
+        traceback.print_exc()
+        return "自動語音辨識發生錯誤"
 
 
-if __name__ == '__main__':
-    launch()
+if __name__ == "__main__":
+    try:
+        # demo.launch(server_name="0.0.0.0", share=True, debug=True, show_error=True)
+        launch()
+    except Exception as e:
+        print(e)
